f3d6483d6f84a400f118f331b39c542fa267151d
Michael Zakany <mzakany@gmail.com>
2015-05-30 01:22:41 -0400

    m

--- parser.rb
+++ parser.rb
@@ -11,6 +11,12 @@ class HtmlTagParser
 		@file_location = file_location	
 	end
 
+	def replace_with_django_tags
+		replace_images
+		replace_css
+		replace_js
+	end
+
 	def replace_images
 		if File.exist?(@html_file)
 			return_formatted_string_html_page_by_tag_type('img').to_html
@@ -20,11 +26,19 @@ class HtmlTagParser
 	end
 
 	def replace_css
-		
+		if File.exist?(@html_file)
+			return_formatted_string_html_page_by_tag_type('css').to_html
+		else
+			raise 'There is nothing to parse'
+		end
 	end
 
 	def replace_js
-
+		if File.exist?(@html_file)
+			return_formatted_string_html_page_by_tag_type('js').to_html
+		else
+			raise 'There is nothing to parse'
+		end
 	end
 
 
@@ -38,9 +52,16 @@ class HtmlTagParser
 	def return_formatted_string_html_page_by_tag_type(tag)
 		if tag == 'img' 
 			tag = " <#{tag}"
+			tag_name = 'img'
+		elsif tag == 'css'
+			tag = '<link rel'
+			tag_name = 'css'
+		elsif tag == 'js'
+			tag = '<script src="js'
+			tag_name = 'js'
 		end
 
-		@tag = tag
+		@tag = tag_name
 		
 		@formatted_output_string = ""
 
@@ -69,8 +90,14 @@ class HtmlTagParser
 						match = return_relavant_tag_regex(whole_line)
 
 						unless match.nil?
-							new_formatted_line = "#{match[:start]} " + %Q["{% static '#{match[:middle]}' %}"] + ' ' + "class='#{match[:class]}' + alt=''"
-					
+							if @tag == 'img'
+								new_formatted_line = "#{match[:start]} " + %Q["{% static '#{match[:middle]}' %}"] + ' ' + "class='#{match[:class]}' + alt=''"
+							elsif @tag == 'css'
+								new_formatted_line = "<link rel='stylesheet' " + %Q["{% static '#{match[:css]}' %}"] + ' ' + "type='text/css'>"
+							elsif @tag == 'js'
+								new_formatted_line = "<script src=" + %Q["{% static '#{match[:css]}' %}">] + "<"	
+							end
+
 							output_string[i..x] = ''
 					
 							@formatted_output_string += new_formatted_line
@@ -89,6 +116,77 @@ class HtmlTagParser
 
 	end
 
+	def return_formatted_string
+		tags_to_look_for = {
+			img: ' <img',
+			css: '<link rel',
+			js: '<script src="js'
+		}
+		
+		output_string = generate_output_string
+		@formatted_output_string = ""
+
+		for i in 0..output_string.length-1
+			tags_to_look_for.each_pair do |short_name,tag|
+				full_word = output_string[i..i+tag.length-1]
+				if full_word == tag 
+					x = i
+					whole_line = ""
+					bracket_count = 0
+					havnt_found_everything_inside_brackets = true
+
+					while havnt_found_everything_inside_brackets do 
+						
+						unless output_string[x].nil?
+
+							whole_line += output_string[x]
+					
+							bracket_count += 1 if output_string[x] == '>'
+
+							if bracket_count != 1
+								x += 1
+							else
+								begin
+									new_formatted_line = return_regex(short_name,whole_line)
+									p new_formatted_line
+									@formatted_output_string += new_formatted_line
+								rescue
+								
+								ensure
+									
+								end
+							
+								havnt_found_everything_inside_brackets = false
+							end
+					
+						end
+					
+					end
+				end
+
+			end
+		end
+		
+		return self
+	end
+
+	def return_regex(short_name,line)
+		whole_line_de_stringed = line.gsub(/\"/, '')
+		
+		if short_name == :img
+			match = /(?<start><img src=)(?<middle>\w{0,4}\/\w{0,25}\/\d{0,10}\.\w{0,4})\s\w{0,9}\=(?<class>\w{0,5}.\w{0,20})(?<all_else>.{0,10})/.match(whole_line_de_stringed)
+			new_formatted_line = "<img src="  "{% static '#{match[:middle]}' %} " "class='#{match[:class]}' + alt=''"
+		elsif short_name == :css
+			match = /\<\w{0,5}.\w{0,4}.\w{0,10}.\w{0,5}\=(?<css>[^.]{0,20}\.[^\s]{0,20})/.match(whole_line_de_stringed)
+			new_formatted_line = "<link rel='stylesheet' " "{% static '#{match[:css]}' %}" "type='text/css'>"
+		elsif short_name == :js
+			match = /\<\w{0,5}.\w{0,4}.\w{0,10}.\w{0,5}\=(?<css>[^.]{0,20}\.[^\s]{0,20})\>/.match(whole_line_de_stringed)
+			new_formatted_line = "<script src=" "{% static '#{match[:css]}' %}" "> <script>"	
+		end
+
+		return new_formatted_line
+	end
+
 	def to_html
 		File.open('file.html','w') {|line| line.write(@formatted_output_string)}
 	end
@@ -105,10 +203,17 @@ class HtmlTagParser
 
 	def return_relavant_tag_regex(line)
 		whole_line_de_stringed = line.gsub(/\"/, '')
+		
 		if @tag == 'img'
 			match = /(?<start><img src=)(?<middle>\w{0,4}\/\w{0,25}\/\d{0,10}\.\w{0,4})\s\w{0,9}\=(?<class>\w{0,5}.\w{0,20})(?<all_else>.{0,10})/.match(whole_line_de_stringed)
+		elsif @tag == 'css'
+			match = /\<\w{0,5}.\w{0,4}.\w{0,10}.\w{0,5}\=(?<css>[^.]{0,20}\.[^\s]{0,20})/.match(whole_line_de_stringed)
+		elsif @tag == 'js'
+			match = /\<\w{0,5}.\w{0,4}.\w{0,10}.\w{0,5}\=(?<css>[^.]{0,20}\.[^\s]{0,20})\>/.match(whole_line_de_stringed)
 		end
+
 		return match
+
 	end
 end
 
